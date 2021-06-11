20f1ac4305e4acdd0bd5c6a52c0eccadbf634c40
romain <mcbess18@gmail.com>
2021-06-11 14:41:36 +0200

    section biography

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="biography")
+     * @return Response
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $biography = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findOneBy([]);
+        return $this->render('home/index.html.twig', ['biography' => $biography]);
     }
 }
