daa63620c30ececce2f5406ad2d923b027d22824
Séverine <severine.coute@laposte.net>
2021-06-17 17:14:04 +0200

    services section

---
+++ src/Controller/IndividualsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndividualsController extends AbstractController
+{
+    /**
+     * @Route("/individuals/index", name="individuals")
+     */
+    public function index(): Response
+    {
+        return $this->render('Individuals/index.html.twig');
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,9 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
+    
     .addEntry('app', './assets/app.js')
+    .addStyleEntry('individuals', './assets/styles/individuals.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
