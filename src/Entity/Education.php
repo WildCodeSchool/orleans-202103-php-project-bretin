0d35da17eaae121e20d9a0bec63c5490b6d344a3
romain <mcbess18@gmail.com>
2021-06-17 11:20:20 +0200

    section education

---
+++ src/Entity/Education.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EducationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EducationRepository::class)
+ */
+class Education
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private int $obtentionYear;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getObtentionYear(): ?int
+    {
+        return $this->obtentionYear;
+    }
+
+    public function setObtentionYear(int $obtentionYear): self
+    {
+        $this->obtentionYear = $obtentionYear;
+
+        return $this;
+    }
+}
