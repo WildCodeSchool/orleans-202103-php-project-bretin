b57df3c711781b567ee41723b9935bd002cc24f8
Séverine <severine.coute@laposte.net>
2021-07-02 10:10:52 +0200

    affichage services

--- src/DataFixtures/ServiceFixtures.php
+++ src/DataFixtures/ServiceFixtures.php
@@ -8,102 +8,22 @@ use App\Entity\Service;
 
 class ServiceFixtures extends Fixture
 {
+
+    public CONST services = ['Fatigue', 'travail', 'troubles du sommeil', 'stress', 'burn out', 'projet', 'mobilité', 'doutes', 'tensions', 'conflits', 'bilan de compétences', 'orientation', 'écoute', 'soutien'];
+
+    
     public function load(ObjectManager $manager)
     {
+     
+        foreach (self::services as $index => $data) {
         $service = new Service();
-        $service->setName("Aide psychologique");
+        $service->setName($data);
         $service->setPrice("50");
         $manager->persist($service);
-        $manager->flush();
+        $this->addReference('service_' . $index, $service);
 
-        $service = new Service();
-        $service->setName("Psychologie travail");
-        $service->setPrice("50");
-        $manager->persist($service);
-        $manager->flush();
+        }
 
-        $service = new Service();
-        $service->setName("Insomnie");
-        $service->setPrice("50");
-        $manager->persist($service);
-        $manager->flush();
-
-        $service = new Service();
-        $service->setName("Dépression");
-        $service->setPrice("50");
-        $manager->persist($service);
-        $manager->flush();
-
-        $service = new Service();
-        $service->setName("Anxiété");
-        $service->setPrice("50");
-        $manager->persist($service);
-        $manager->flush();
-
-        $service = new Service();
-        $service->setName("Harcèlement travail");
-        $service->setPrice("50");
-        $manager->persist($service);
-        $manager->flush();
-
-        $service = new Service();
-        $service->setName("Stress");
-        $service->setPrice("50");
-        $manager->persist($service);
-        $manager->flush();
-
-        $service = new Service();
-        $service->setName("Personnes handicapées");
-        $service->setPrice("50");
-        $manager->persist($service);
-        $manager->flush();
-
-        $service = new Service();
-        $service->setName("Burn-out");
-        $service->setPrice("50");
-        $manager->persist($service);
-        $manager->flush();
-
-        $service = new Service();
-        $service->setName("Test de personnalité");
-        $service->setPrice("50");
-        $manager->persist($service);
-        $manager->flush();
-
-        $service = new Service();
-        $service->setName("Test psychotechnique");
-        $service->setPrice("50");
-        $manager->persist($service);
-        $manager->flush();
-
-        $service = new Service();
-        $service->setName("Test d'orientation professionnelle");
-        $service->setPrice("50");
-        $manager->persist($service);
-        $manager->flush();
-
-        $service = new Service();
-        $service->setName("Test de recrutement");
-        $service->setPrice("50");
-        $manager->persist($service);
-        $manager->flush();
-
-        $service = new Service();
-        $service->setName("Développement personnel");
-        $service->setPrice("50");
-        $manager->persist($service);
-        $manager->flush();
-
-        $service = new Service();
-        $service->setName("Confiance en soi");
-        $service->setPrice("50");
-        $manager->persist($service);
-        $manager->flush();
-
-        $service = new Service();
-        $service->setName("Aptitudes sociales");
-        $service->setPrice("50");
-        $manager->persist($service);
         $manager->flush();
     }
 }
