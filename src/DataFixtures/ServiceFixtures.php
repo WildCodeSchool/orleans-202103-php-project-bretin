3c9d1b6f47182f284fbbecf7c857c60dac7b021d
Séverine <severine.coute@laposte.net>
2021-07-05 11:14:34 +0200

    affichage services

---
+++ src/DataFixtures/ServiceFixtures.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Service;
+
+class ServiceFixtures extends Fixture
+{
+
+    public const SERVICES = [
+        'Fatigue',
+        'travail',
+        'troubles du sommeil',
+        'stress',
+        'burn out',
+        'projet',
+        'mobilité',
+        'doutes',
+        'tensions',
+        'conflits',
+        'bilan de compétences',
+        'orientation',
+        'écoute',
+        'soutien'
+    ];
+
+
+    public function load(ObjectManager $manager)
+    {
+
+        foreach (self::SERVICES as $index => $data) {
+            $service = new Service();
+            $service->setName($data);
+            $service->setPrice("50");
+            $service->setPrice2("0");
+            $manager->persist($service);
+            $this->addReference('service_' . $index, $service);
+        }
+
+        $manager->flush();
+    } 
+}
