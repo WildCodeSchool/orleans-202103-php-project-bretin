20ac2faf868c4fd1125c9ce9121cc90cee5b7b27
Séverine <severine.coute@laposte.net>
2021-07-08 18:47:09 +0200

    suppression service et message flash

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -87,6 +87,9 @@ class ServiceController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($service);
             $entityManager->flush();
+
+            $this->addFlash('success', 'Le service a bien été supprimé.');
+            return $this->redirectToRoute('service_index');
         }
 
         return $this->redirectToRoute('service_index');
