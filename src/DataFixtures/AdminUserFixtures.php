50149c152a9baf3ea599684ac01ccfef692c5f14
Séverine <severine.coute@laposte.net>
2021-07-26 15:00:33 +0200

    fix_styles_font_crud

--- src/DataFixtures/AdminUserFixtures.php
+++ src/DataFixtures/AdminUserFixtures.php
@@ -26,13 +26,17 @@ class AdminUserFixtures extends Fixture
         $admin->setBiography('
         L’expérience du travail transforme.
         Les difficultés vécues et exprimées à l’occasion ou autour du travail m’ont toujours questionnées.
-        Mes expériences et rencontres ont nourri cet intérêt, Pourquoi certains collègues tombent malade ? Pourquoi le 
-        travail rend malade ? Pourquoi ces tensions et conflits ? Pourquoi ces difficultés à avancer ? Pourquoi
-        ces symptômes ?
+        
+        Mes expériences et rencontres ont nourri cet intérêt, Pourquoi certains collègues tombent malade ? 
+        Pourquoi le travail rend malade ? Pourquoi ces tensions et conflits ? 
+        Pourquoi ces difficultés à avancer ? Pourquoi ces symptômes ?
+
         Issu du monde de l’emploi, de l’accompagnement et de la formation, j’ai orienté ma carrière vers l’écoute et le 
         soutien à la personne.
+        
         Ayant eu une activité salariée, ayant travaillé dans différentes organisations, à la fois publiques et privées, 
-        c’est avec une réelle compréhension et connaissance des situations de travail que je vous accompagne et soutien.
+        c’est avec une réelle compréhension et connaissance des situations de travail que je vous accompagne et soutiens
+
         Titulaire du titre de psychologue du travail, certifié sur le MBTI niveau 1 et niveau 2, j’aborde mon activité 
         d’écoute et de soutien avec une connaissance des paradoxes, tensions, impasses, empêchements du travail.
         Je vous accompagne également sur les sphères qui s’articulent avec celles du travail.
