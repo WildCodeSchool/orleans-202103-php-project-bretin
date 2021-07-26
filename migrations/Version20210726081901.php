55c720ee50950c1db0d8f805a9f1949d73e18f5f
jfillion12394 <jfillion12394@gmail.com>
2021-07-26 13:25:19 +0200

    fix erreur interne au lencement de la prod à cause données vides dans biographie

--- migrations/Version20210726081901.php
+++ migrations/Version20210726081901.php
@@ -19,8 +19,26 @@ final class Version20210726081901 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $tempPassword = '$argon2id$v=19$m=65536,t=4,p=1$MjhP0ITvZ7nwAC6sMG+ifQ$9HqPdT62Yy4HvvSH1z5Ez0zSi086ds0PypZTn9nRCZ4';
-        $this->addSql( "INSERT INTO admin_user (email,roles,password, biography, url) values ('cabinet.bretin@gmail.com', '[\"ROLE_ADMIN\"]','$tempPassword','','')" );
+
+        $bioUrl = '20210622-203950-60e81212021f1033095278.webp';
+        $bioText = 'Issu du monde du travail et de l’entreprise, ayant évolué dans le secteur public et privé, j’ai évolué sur des 
+        fonctions managériales en ressources humaines.
+        L’expérience du travail transforme.
+        Les difficultés vécues et exprimées à l’occasion ou autour du travail m’ont toujours questionnées.
+        Mes expériences et rencontres ont nourri cet intérêt, Pourquoi certains collègues tombent malade ? Pourquoi le 
+        travail rend malade ? Pourquoi ces tensions et conflits ? Pourquoi ces difficultés à avancer ? Pourquoi
+        ces symptômes ?
+        Issu du monde de l’emploi, de l’accompagnement et de la formation, j’ai orienté ma carrière vers l’écoute et le 
+        soutien à la personne.
+        Je vous recevrai donc avec une réelle compréhension et connaissance des expériences et problématiques que vous 
+        avez vécues. 
+        Titulaire du titre de psychologue du travail, certifié sur le MBTI niveau et niveau 2, j’aborde mon activité 
+        d’écoute et de soutien avec une connaissance des paradoxes, tensions, impasses, empêchements du travail,
+        pour y avoir été également exposé.
+        Je vous accompagne également sur les sphères qui s’articulent avec celles du travail.
+        ';
+
+        $this->addSql( "INSERT INTO admin_user (email,roles,password, biography, url) values ('', '[\"\"]','','$bioText', '$bioUrl')" );
     }
 
     public function down(Schema $schema): void
