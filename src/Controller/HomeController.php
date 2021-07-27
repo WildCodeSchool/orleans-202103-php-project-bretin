e8f61f6e6369fa8256c89dc00bb02317951e58f0
wilder <NEL45>
2021-07-27 16:23:56 +0200

    Footer BDD relation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Repository\EducationRepository;
 use App\Repository\AdminUserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\EducationRepository;
+use App\Repository\OfficeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -15,13 +16,18 @@ class HomeController extends AbstractController
      * @Route("/", name="accueil")
      * @return Response
      */
-    public function index(AdminUserRepository $userRepository, EducationRepository $educationRepository): Response
-    {
+    public function index(
+        AdminUserRepository $userRepository,
+        EducationRepository $educationRepository,
+        OfficeRepository $officeRepository
+    ): Response {
         $user = $userRepository->findOneBy([]);
         $education = $educationRepository->findBy([], ['obtentionYear' => 'DESC']);
+        $office = $officeRepository->findAll();
         return $this->render('home/index.html.twig', [
             'user' => $user,
-            'educations' => $education
+            'educations' => $education,
+            'offices' => $office,
         ]);
     }
 }
