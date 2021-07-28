3972b8a3b8f5c565f6bc5fc7f61f5dbb4449f0e0
wilder <NEL45>
2021-07-28 10:51:48 +0200

    Fix offices

--- src/Controller/IndividualsController.php
+++ src/Controller/IndividualsController.php
@@ -2,21 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\OfficeRepository;
 use App\Repository\ServiceRepository;
 use App\Repository\TestimonyRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IndividualsController extends AbstractController
 {
     /**
      * @Route("/particuliers", name="particuliers")
      */
-    public function index(serviceRepository $serviceRepository, testimonyRepository $testimonyRepository): Response
-    {
-
+    public function index(
+        serviceRepository $serviceRepository,
+        testimonyRepository $testimonyRepository,
+        OfficeRepository $officeRepository
+    ): Response {
         return $this->render('Individuals/index.html.twig', [ "services" => $serviceRepository->findAll(),
-        "testimonies" => $testimonyRepository->findBy([], ['date' => 'DESC'])]);
+        "testimonies" => $testimonyRepository->findBy([], ['date' => 'DESC']),
+        'offices' => $officeRepository->findAll(),
+        ]);
     }
 }
